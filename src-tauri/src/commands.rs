901f791df0283cb417c60b18f2ba79c10c433d46
GuilAFT <guilaft@riseup.net>
2023-01-25 19:06:31 -0300

    feat: return command errors to frontend

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -2,42 +2,45 @@ use crate::api;
 use crate::database;
 use crate::models;
 
-#[tauri::command]
-pub fn get_people(criteria: api::people::ListPeopleCriteria) -> Vec<models::Person>{
-    let connection = &mut database::establish_connection();
+type CommandResult<T> = Result<T, String>;
 
-    api::people::get_people(connection, &criteria)
-        .expect("Could not get people")
+macro_rules! handle_return {
+    ($action:expr) => {
+        $action.map_err(|e| e.to_string())
+    };
 }
 
 #[tauri::command]
-pub fn get_people_by_id(id: i32) -> models::Person {
+pub fn get_people(criteria: api::people::ListPeopleCriteria) -> CommandResult<Vec<models::Person>> {
     let connection = &mut database::establish_connection();
 
-    api::people::get_person_by_id(connection, id)
-        .expect("Could not get person by id")
+    api::people::get_people(connection, &criteria).map_err(|e| e.to_string())
 }
 
 #[tauri::command]
-pub fn create_person(new_person: models::NewPerson) -> usize {
+pub fn get_people_by_id(id: i32) -> CommandResult<models::Person> {
     let connection = &mut database::establish_connection();
 
-    api::people::create_person(connection, new_person)
-        .expect("Could not create person")
+    handle_return!(api::people::get_person_by_id(connection, id))
 }
 
 #[tauri::command]
-pub fn update_person(id: i32, updated_person: models::Person) -> usize {
+pub fn create_person(new_person: models::NewPerson) -> CommandResult<usize> {
     let connection = &mut database::establish_connection();
 
-    api::people::update_person(connection, id, updated_person)
-        .expect("Could not update person")
+    handle_return!(api::people::create_person(connection, new_person))
 }
 
 #[tauri::command]
-pub fn delete_person(id: i32) -> usize {
+pub fn update_person(id: i32, updated_person: models::Person) -> CommandResult<usize> {
     let connection = &mut database::establish_connection();
 
-    api::people::delete_person(connection, id)
-        .expect("Could not delete person")
+    handle_return!(api::people::update_person(connection, id, updated_person))
+}
+
+#[tauri::command]
+pub fn delete_person(id: i32) -> CommandResult<usize> {
+    let connection = &mut database::establish_connection();
+
+    handle_return!(api::people::delete_person(connection, id))
 }
