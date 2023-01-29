dc5ab312b3ba787ebc2188a4950a5046fe3057da
GuilAFT <guilaft@riseup.net>
2023-01-29 08:27:59 -0300

    feat: add local data dir to store database

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -8,8 +8,21 @@ pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    // TODO: Find a location in file system for database.db (.local/share/ccvv)
-    let database_url = env::var("DATABASE_URL").unwrap_or("database.db".to_string());
+    let project_dir = directories::ProjectDirs::from("com", "ccvv", "ccvv")
+        .expect("Could not get local data dir");
+
+    let local_data_dir = project_dir.data_local_dir();
+
+    if !local_data_dir.exists() {
+        std::fs::create_dir_all(local_data_dir).expect("Could not create local data dir")
+    }
+
+    let local_database_path = local_data_dir
+        .join("database.db")
+        .to_string_lossy()
+        .to_string();
+
+    let database_url = env::var("DATABASE_URL").unwrap_or(local_database_path);
 
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
